19e5c9c7a61c94bc7b7e7c2841b41ab2e9dd2cf9
fasaeed <fasaeed@ucsc.edu>
2019-10-31 11:31:12 -0700

    get Games works, and output to results.csv

--- webscrape/getGames.py
+++ webscrape/getGames.py
@@ -1,16 +1,45 @@
 import requests
 import csv
 
+
 url = "https://api-v3.igdb.com/games"
 
-payload = "fields name,platforms;\nwhere platforms = 48;\noffset 49;\nlimit 50;\n"
-headers = {
-    'user-key': "65870b1858a85d7e30df9a6d9c40fdd0",
-    'cache-control': "no-cache",
-    'Postman-Token': "b4537987-904d-42e7-9af6-1b230d933f9c"
+
+def scrape(platform, offset, limit):
+
+    payload = "fields name,platforms;\nwhere platforms = %d;\noffset %d;\nlimit %d;\n" % (platform, offset, limit)
+    # print(payload)
+    headers = {
+        'user-key': "65870b1858a85d7e30df9a6d9c40fdd0",
+        'cache-control': "no-cache",
+        'Postman-Token': "b4537987-904d-42e7-9af6-1b230d933f9c"
     }
 
-response = requests.request("GET", url, data=payload, headers=headers)
-sol = (response.json()[0]['id'])
+    response = requests.request("GET", url, data=payload, headers=headers)
+    sol = (response.json())
 
-print(sol)
+    ids = []
+    for item in sol:
+        ids.append(item['id'])
+
+    # print('ids in')
+    # print(ids)
+    return ids
+
+    
+
+
+if __name__ == '__main__':
+    arr = []
+    for i in range(0, 1000, 50):
+        arr.append(scrape(24, i, 50))
+
+    # print(arr)
+    resultFile = open("results.csv",'w')
+    wr = csv.writer(resultFile, delimiter=";")
+    for items in arr:
+        wr.writerows(map(lambda x: [x], items))
+    resultFile.close()
+
+    
+
